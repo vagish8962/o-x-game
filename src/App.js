7de863127a26e448f5326284201c3165bcc877a7
gupta, Vagish <vagish.gupta@netapp.com>
2024-05-09 12:24:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+const WINNING_PATTERNS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+const initialState = Array(9).fill(null);
+
 function App() {
+  const [board, setBoard] = useState(initialState);
+  const [isXTurn, setIsXTurn] = useState(false);
+
+  const calculateWinner = () => {
+    for(let i = 0; i < WINNING_PATTERNS.length; i++) {
+      const [a, b, c] = WINNING_PATTERNS[i];
+      if(board[a] && board[a]=== board[b] && board[b]=== board[c]) {
+        return board[a];
+      }
+    }
+    return null
+  }
+
+  const boxHandleClick = (i) => {
+    const winner = calculateWinner();
+    if(winner) return;
+    setIsXTurn(!isXTurn);
+    const cloneboard = [...board];
+    cloneboard[i] = isXTurn ? 'X' : 'O';
+    setBoard(cloneboard);
+   
+  }
+
+  const getStatusMessage = () => {
+    const winner = calculateWinner(board);
+    if (winner) return `Player ${winner} wins!`;
+    if (!board.includes(null)) return `It's a draw!`;
+    return `Player ${isXTurn ? "X" : "O"} turn`;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="game">
+      <h1>{getStatusMessage()}</h1>
+        <div className='board'>
+            {
+              board.map((b, i) => <button className='box' onClick={() => boxHandleClick(i)} key={i}  disabled={b !== null}
+              >{b}</button> )
+            }
+        </div>
     </div>
   );
 }
